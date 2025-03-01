bb90d9eafddb75d07b7c48abe124970a1307675b
TheingiThaw <theingithaw03@gmail.com>
2025-03-01 21:32:42 +0630

    end-day commit

--- resources/js/App/CustomAudioPlayer.jsx
+++ resources/js/App/CustomAudioPlayer.jsx
@@ -1,14 +1,15 @@
+import { PauseCircleIcon, PlayCircleIcon } from '@heroicons/react/16/solid';
 import React, { useRef, useState } from 'react'
 
-const CustomAudioPlayer = () => {
+const CustomAudioPlayer = ({ file, showVolume = false }) => {
     const [duration, setDuration] = useState();
     const [currentTime, setCurrentTime] = useState();
     const [volume, setVolume] = useState();
     const [isPlaying, setIsPlaying] = useState(false);
     const audioCtr = useRef();
-    const audio = audioCtr.current;
 
     const togglePlayPause = () => {
+        const audio = audioCtr.current;
         if (isPlaying) {
             audio.pause();
         }
@@ -22,6 +23,7 @@ const CustomAudioPlayer = () => {
     }
 
     const volumeChange = (e) => {
+        const audio = audioCtr.current;
         const volume = e.target.value;
         audio.current.volume = volume;
         setVolume(volume);
@@ -31,10 +33,54 @@ const CustomAudioPlayer = () => {
         setDuration(e.target.value);
     };
 
+    const timeUpdate = (e) => {
+        const audio = audioCtr.current;
+        setDuration(audio.duration);
+        setCurrentTime(e.target.currentTime);
+    }
+
+    const handleChange = (e) => {
+        const audio = audioCtr.current;
+        const time = e.target.value;
+        audio.current.currentTime = time;
+        setCurrentTime(time);
+    }
 
     return (
-        <div className='w-full '>
+        <div className='w-full gap-2 rounded-md bg-black/20 flex items-center'>
+            <audio
+                ref={audioCtr}
+                src={file.url}
+                controls
+                onTimeUpdate={timeUpdate}
+                onLoadedMetadata={handleLoadedMetaData}
+                className='hidden'
+            />
 
+            <button onClick={togglePlayPause}>
+                {isPlaying && <PauseCircleIcon className='w-6 h-6 text-gray-400' />}
+                {!isPlaying && <PlayCircleIcon className='w-6 h-6 text-gray-400' />}
+            </button>
+
+            {showVolume && (
+                <input
+                    type='range'
+                    min='0'
+                    max='1'
+                    step='0.01'
+                    value={volume}
+                    onChange={volumeChange}
+                />
+            )}
+
+            <input
+                type='range'
+                min='0'
+                max={duration}
+                step='0.01'
+                value={currentTime}
+                onChange={handleChange}
+            ></input>
         </div>
     )
 }
